31bb837e3539f8c05c203f5d46a12af5fc2ea9e6
chrisvandergeer <chrisvandergeer@gmail.com>
2022-05-10 20:55:43 +0200

    Zoeken transakties en budgeteren in 1 scherm

--- src/main/java/nl/cge/mpfinance/control/FindTransaktiesController.java
+++ src/main/java/nl/cge/mpfinance/control/FindTransaktiesController.java
@@ -6,6 +6,8 @@ import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
 import jakarta.persistence.criteria.Predicate;
 import jakarta.persistence.criteria.Root;
+import nl.cge.mpfinance.entity.Budgetregel;
+import nl.cge.mpfinance.entity.BudgetregelMetTransaktiesDto;
 import nl.cge.mpfinance.entity.FindTransaktiesDto;
 import nl.cge.mpfinance.entity.Transaktie;
 
@@ -17,24 +19,28 @@ public class FindTransaktiesController {
     @PersistenceContext
     private EntityManager entityManager;
 
-    public List<Transaktie> find(FindTransaktiesDto findTransaktiesDto) {
+    public BudgetregelMetTransaktiesDto find(FindTransaktiesDto findTransaktiesDto) {
         CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
         CriteriaQuery<Transaktie> criteriaQuery = criteriaBuilder.createQuery(Transaktie.class);
         Root<Transaktie> transaktieRoot = criteriaQuery.from(Transaktie.class);
         List<Predicate> predicateList = new ArrayList<>();
-        if (findTransaktiesDto.getTegenrekening() != null) {
+        if (findTransaktiesDto.getTegenrekening() != null && !"".equals(findTransaktiesDto.getTegenrekening().trim())) {
             predicateList.add(criteriaBuilder.equal(transaktieRoot.get("tegenrekening"), findTransaktiesDto.getTegenrekening()));
         }
-        if (findTransaktiesDto.getNaamTegenpartij() != null) {
+        if (findTransaktiesDto.getNaamTegenpartij() != null && !"".equals(findTransaktiesDto.getNaamTegenpartij().trim())) {
             predicateList.add(criteriaBuilder.equal(transaktieRoot.get("naamTegenpartij"), findTransaktiesDto.getNaamTegenpartij()));
         }
-        if (findTransaktiesDto.getOmschrijving() != null) {
+        if (findTransaktiesDto.getOmschrijving() != null && !"".equals(findTransaktiesDto.getOmschrijving().trim())) {
             predicateList.add(criteriaBuilder.like(transaktieRoot.get("omschrijving"), "%" + findTransaktiesDto.getOmschrijving() + "%"));
         }
         criteriaQuery.where(predicateList.toArray(new Predicate[]{}));
         criteriaQuery.orderBy(criteriaBuilder.desc(transaktieRoot.get("volgnr")));
-        return entityManager.createQuery(criteriaQuery)
-                .setMaxResults(200)
-                .getResultList();
+        return new BudgetregelMetTransaktiesDto().setTransakties(entityManager.createQuery(criteriaQuery)
+                        .setMaxResults(200)
+                        .getResultList())
+                .setBudgetregel(new Budgetregel()
+                        .setOmschrijving(findTransaktiesDto.getOmschrijving())
+                        .setTegenrekening(findTransaktiesDto.getTegenrekening())
+                        .setNaamTegenpartij(findTransaktiesDto.getNaamTegenpartij()));
     }
 }
--- src/main/java/nl/cge/mpfinance/entity/Budgetregel.java
+++ src/main/java/nl/cge/mpfinance/entity/Budgetregel.java
@@ -14,7 +14,7 @@ public class Budgetregel {
     private String budgetregelNaam;
     private String tegenrekening;
     private String naamTegenpartij;
-    private String omschrijvingBevat;
+    private String omschrijving;
     private String budget;
     private String soort;
 }
